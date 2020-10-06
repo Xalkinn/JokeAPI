c4b288cec70aa150a21147c67fa3c69b304591b5
ADE17335 <ADE17335@REN1DSK0123.ouest.teamlog.intra>
2020-10-06 11:42:44 +0200

    ADE : Mise en place du projet Spring

---
+++ src/main/java/joke/api/test/JokeApiSpringApplication.java
@@ -0,0 +1,13 @@
+package joke.api.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/joke/api/test/JokeApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package joke.api.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
